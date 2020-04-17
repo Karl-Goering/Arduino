effc4eb382de4c4048cd99a554d90be8a9a7075a
Purestream <elemtype8964@gmail.com>
2020-04-17 20:10:41 +0800

    Add files via upload

---
+++ potentiometer_servo.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+Servo myservo; 
+int sensor=0; 
+int val;
+void setup() {
+myservo.attach(9); 
+}
+void loop() {
+sensor=analogRead(A0);  
+val = map(sensor, 0, 1023, 0, 180);  
+myservo.write(val); 
+delay(15); 
+}
